f2ad61b57245cfb77faa265525c09abb08a4f60b
Asya Dyadyuk <drasyady@gmail.com>
2022-11-01 13:10:58 +0200

    ui of the popup

--- 2bPrecise/employee-project/src/Modal.js
+++ 2bPrecise/employee-project/src/Modal.js
@@ -4,7 +4,17 @@ import { useState } from 'react';
 import './Modal.css';
 
 function Modal({ closeModal }) {
-
+    const [date, setDate] = useState();
+    const [task, setTask] = useState("");
+    console.log("Date", date);
+    function handelChange(event) {
+        setTask(prevFromTask => {
+            return {
+                ...prevFromTask,
+                // [event.target.name]: event.target.value
+            }
+        })
+    }
     return (
         <div className='modalBackground'>
             <div className='modalContainer'>
@@ -12,7 +22,11 @@ function Modal({ closeModal }) {
                     <button onClick={() => closeModal(false)}>X</button>
                 </div>
                 <div className='body'></div>
-                <p>The Next...</p>
+                <textarea className='textarea'
+                    onChange={handelChange}
+                    placeholder="Write your task here..." />
+                <h4 className='date'>Due Date: {date}</h4>
+                <input type="date" onChange={e => setDate(e.target.value)} />
                 <div className='footer'>
                     <button>Save</button>
                     <button onClick={() => closeModal(false)}>Cancel</button>
